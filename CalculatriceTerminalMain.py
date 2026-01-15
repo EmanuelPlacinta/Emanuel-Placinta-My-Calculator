061c9d8a476e30c218a3a4c697580425d04ba5b3
Michel <michelrostain@laplateforme.io>
2026-01-15 13:06:56 +0100

    Version avec racine carrée

--- CalculatriceTerminalMain.py
+++ CalculatriceTerminalMain.py
@@ -1,58 +1,121 @@
 import re
+from collections import deque
+import os
+import time
+
+# L'historique doit être défini à l'extérieur pour être accessible partout
+historique = deque(maxlen=10)
 
 def menu():
-    print("Veuillez rentrer un choix : ")
+    print("\n--- MENU ---")
     print("1 / Calculer" )
     print("2 / Afficher l'historique")
     print("3 / Effacer l'historique")
-    choix=(input("Rentrez votre choix : "))
+    print("4 / Quitter")
+    choix = input("Rentrez votre choix : ")
     return choix
 
-def calculer():
-    priorite = {'(':0, '+': 1, '-': 1, '*': 2, '/': 2}
-    nombres=[]
-    operateur=[]
-    calcul=input("Veuillez rentrer votre calcul : ")
-    liste_calcul = re.findall(r"\d+\.?\d*|[-+*/()]", calcul)
-    for i in liste_calcul:
+def afficher_historique():
+    os.system('clear')
+    if not historique:
+        print("Aucun calcul dans l'historique")
+        time.sleep(2)
+        os.system('clear')
+    else:
+        os.system('clear')
+        print("\n--- Historique (10 derniers) ---")
+        for i, (c, r) in enumerate(historique, 1):
+            print(f"{i}. {c} = {r}")
+        input("Appuyer sur entrée pour continuer...")
+
+def calculer(expression_a_calculer): # On donne un nom clair à l'entrée
+    priorite = {'(':0, '+': 1, '-': 1, '*': 2, '/': 2, '**':3, 'v':5}
+    nombres = []
+    operateur = []
+    
+    # Utilisation de l'expression passée en argument
+    liste_calcul = re.findall(r"v|\d+\.?\d*|[-+*/()]", expression_a_calculer)
+    
+    for index, i in enumerate(liste_calcul):
         if i.replace(".", "", 1).isdigit():
             nombres.append(i)
+
+        elif i == '-':
+            # Cas 1 : Le calcul commence par "-" (ex: -5 + 2)
+            # Cas 2 : Le "-" est juste après une parenthèse (ex: 5 * (-2))
+            if index == 0 or liste_calcul[index-1] == '(':
+                nombres.append('0') # On insère un 0 virtuel
+            
+            # Puis on traite le "-" comme un opérateur normal
+            while operateur and priorite[operateur[-1]] >= priorite[i]:
+                nombres.append(operateur.pop())
+            operateur.append(i)       
+
+        elif i == '(':
+            operateur.append(i)
+
+        elif i == ')': 
+            while operateur and operateur[-1] != '(':
+                nombres.append(operateur.pop())
+            if operateur:
+                operateur.pop()
 
         elif i in priorite:
             while operateur and priorite[operateur[-1]] >= priorite[i]:
                 nombres.append(operateur.pop())
             operateur.append(i)
-        
-        elif i == '(':
-            operateur.append(i)
-        
-        elif i == ')' : 
-            while operateur and priorite[operateur[-1]] != '(':
-                nombres.append(operateur.pop())
-            if operateur:
-                operateur.pop()
     
     while operateur:
-        nombres.append(operateur.pop())
+        symbole = operateur.pop()
+        if symbole != '(':
+            nombres.append(symbole)
     
+    # Calcul final
     calcul_final = []
     for i in nombres:
+
         if i.replace(".", "", 1).isdigit():
             calcul_final.append(float(i))
-        else: # On ne calcule QUE si c'est un opérateur
-            nb1 = calcul_final.pop() # Chiffre de droite
-            nb2 = calcul_final.pop() # Chiffre de gauche
+        else: 
+            nb1 = calcul_final.pop()
+            if i == 'v':
+                calcul_final.append(nb1**0.5)
+            else :
+                nb2 = calcul_final.pop()
 
             if i == '+': calcul_final.append(nb2 + nb1)
             elif i == '-': calcul_final.append(nb2 - nb1)
             elif i == '*': calcul_final.append(nb2 * nb1)
             elif i == '/': calcul_final.append(nb2 / nb1)
+            elif i == '**': calcul_final.append(nb2**nb1)
 
     return calcul_final[0]
 
 def main():
-    choix_menu=menu()
-    if choix_menu=="1":
-        print(calculer())
+    while True: # La boucle englobe tout le menu
+        os.system('clear')
+        choix_menu = menu()
+        
+        if choix_menu == "1":
+            saisie = input("Veuillez rentrer votre calcul : ")
+            try:
+                resultat = calculer(saisie)
+                print(f"Résultat : {resultat}")
+                input("Appuyez sur entrée pour continuer...")
+                historique.append((saisie, resultat)) # On ajoute directement à la deque
+            except Exception as e:
+                print(f"Erreur dans le calcul : {e}")
+            
+        elif choix_menu == "2":
+            afficher_historique()
+            
+        elif choix_menu == "3":
+            historique.clear()
+            print("Historique effacé")
+            
+        elif choix_menu == "4":
+            print("Au revoir !")
+            break
 
-main()
+if __name__ == "__main__":
+    main()
